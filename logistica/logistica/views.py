b659520c5ac07acf89d961932f00623602dd8efc
Alex <ajs476@nau.edu>
2015-04-12 15:21:42 -0700

    deleted employee.html

--- logistica/logistica/views.py
+++ logistica/logistica/views.py
@@ -100,7 +100,7 @@ def user_login(request):
                 # If the account is valid and active, we can log the user in.
                 # We'll send the user back to the homepage.
                 login(request, user)
-                return HttpResponseRedirect('/employee')
+                return HttpResponseRedirect('/administrator')
             else:
                 # An inactive account was used - no logging in!
                 return HttpResponse('/invalid')
