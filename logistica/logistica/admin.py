51e1152ef62fa1afe75074537735368514baf4bd
cap377 <cap377@nau.edu>
2015-04-18 17:03:43 -0700

    EVALUATION WORKS!

---
+++ logistica/logistica/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from models import Evaluation
+##from models import Contest
+##from models import Refer
+##from models import Signup
+##from models import Sweetsleds
+##from models import Logos
+##from models import Smartpets
+
+admin.site.register(Evaluation)
+##admin.site.register(Contest)
+##admin.site.register(Refer)
+##admin.site.register(Signup)
+##admin.site.register(Sweetsleds)
+##admin.site.register(Logos)
+##admin.site.register(Smartpets)
